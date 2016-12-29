fb1a100c64f00bdd386792eb6b238fd970e5c465
meljohndelbarrio <meljdelbarrio@yahoo.com>
2016-12-30 08:49:51 +1300

    Added pages and services to  the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { IntroPage } from '../pages/intro/intro';
+import { ChecklistsPage } from '../pages/checklist/checklist';
+import { Data } from '../providers/data';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+      IntroPage,
+      ChecklistsPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,8 +20,10 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+      IntroPage,
+      ChecklistsPage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, Storage, Data]
 })
 export class AppModule {}

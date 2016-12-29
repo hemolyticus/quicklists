ca5141b2125563344ce58965c7e66dd9d9b66344
Mel John del Barrio <meljohndelbarrio@mels-macbook-pro.local>
2016-12-30 11:04:21 +1300

    Add Checklist function

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, Platform } from 'ionic-angular';
+import { ChecklistsPage } from '../checklists/checklists';
+import { ChecklistsModel } from '../../models/checklists-model';
+import { Data } from '../../providers/data';
+import { Keyboard } from 'ionic-native';
 
 @Component({
   selector: 'page-home',
@@ -8,8 +11,79 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    //Variables
+    checklists: ChecklistsModel[] =[];
+
+    //Contructor
+  constructor(public navCtrl: NavController, public dataService: Data, public alertCtrl: AlertController, public platform: Platform) {
     
   }
+    //Methods
+  ionViewDidLoad()
+  {
+
+  }
+
+  addChecklist():void
+  {
+
+      let prompt = this.alertCtrl.create
+      (
+          {
+              title: 'New Checklist',
+              message: 'Enter the name of your new checklist below',
+              inputs: [
+                  {
+                      name: 'name'
+                  }
+              ],
+
+              buttons: [
+                  {
+                      text: 'Cancel'
+                  },
+                  {
+                      text: 'Save',
+                      handler: data =>{
+                          let newChecklist = new ChecklistsModel(data.name, []);
+                          this. checklists.push(newChecklist);
+
+                          newChecklist.checklist.subscribe(update => {
+                              this.save();
+                          });
+
+                          this.save();
+                      }
+
+                  }
+              ]
+          }
+      );
+
+      prompt.present();
+
+  }
+
+  renameChecklist(checklist): void
+  {
+
+  }
+
+  viewChecklist(checklist): void
+  {
+
+  }
+
+  removeChecklist(checklist): void
+  {
+
+  }
+
+  save():void
+  {
+
+  }
+
+
 
 }

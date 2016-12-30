c4d0bfb75131c83806834b8f97e541d67185ade6
meljohndelbarrio <meljdelbarrio@yahoo.com>
2016-12-30 15:37:02 +1300

    Rename Checklist function

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -24,7 +24,7 @@ export class HomePage {
 
   }
 
-  addChecklist():void
+  addChecklists():void
   {
 
       let prompt = this.alertCtrl.create
@@ -66,6 +66,34 @@ export class HomePage {
 
   renameChecklist(checklist): void
   {
+      let prompt = this.alertCtrl.create({
+          title: 'Rename Checklist',
+          message: 'Enter the new name of this checklist below',
+          inputs: [
+              {
+                  name: 'name'
+              }
+          ],
+          buttons: [
+              {
+                  text: 'Cancel'
+              },
+              {
+                  text: 'Save',
+                  handler: data =>{
+                      let index = this. checklists.indexOf(checklist);
+
+                      if ( index > -1)
+                      {
+                          this.checklists[index].setTitle(data.name);
+                          this.save();
+                      }
+                  }
+              }
+          ]
+      });
+
+      prompt.present();
 
   }
 

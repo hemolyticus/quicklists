49e8aaa8a18190388e18f3162f79816435c132db
meljohndelbarrio <meljdelbarrio@yahoo.com>
2016-12-31 16:15:37 +1300

    Added goToHome()

--- src/pages/intro/intro.ts
+++ src/pages/intro/intro.ts
@@ -1,22 +1,33 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
+import { HomePage } from '../home/home';
 
-/*
-  Generated class for the Intro page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-intro',
   templateUrl: 'intro.html'
 })
+
 export class IntroPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+    //Variables
+    slideOptions:any;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad IntroPage');
+    //Constructor
+  constructor(public nav: NavController)
+  {
+      this.slideOptions =
+          {
+              pager:true
+          };
+
   }
+
+  goToHome():void
+  {
+      this.nav.setRoot(HomePage);
+  }
+
+
 
 }
